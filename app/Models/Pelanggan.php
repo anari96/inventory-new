c7d25f2a93cf407c692c8e5dae0e1a7b281d7fbd
anari96 <nabilrivaldy@gmail.com>
2023-08-30 10:49:32 +0800

    add soft delete

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pelanggan extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $fillable = [
       "nama_pelanggan",
       "email_pelanggan",
